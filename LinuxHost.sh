4fe4ce0787bbcc667030284c1e6b31749ab87626
Beast1610 <singhharsan@gmail.com>
2024-05-30 15:54:39 +0530

    Tested the scripts and passed

---
+++ LinuxHost.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+
+# Variables to store the checklist status
+CHECKLIST=()
+
+# Prompt for SSH username
+read -p "Enter the SSH username used for Software: " ssh_user
+
+# Function to check resource requirements
+check_resources() {
+    echo "Checking resource requirements..."
+
+    # Check if bc is installed
+    if ! command -v bc &> /dev/null; then
+        echo "bc is not installed. Installing bc..."
+        sudo apt-get install -y bc
+        if [ $? -ne 0 ]; then
+            echo "Failed to install bc."
+            CHECKLIST+=("bc installation: Failed")
+            return 1
+        fi
+    fi
+
+    # Check if sysstat is installed for mpstat
+    if ! command -v mpstat &> /dev/null; then
+        echo "sysstat is not installed. Installing sysstat..."
+        sudo apt-get install -y sysstat
+        if [ $? -ne 0 ]; then
+            echo "Failed to install sysstat."
+            CHECKLIST+=("sysstat installation: Failed")
+            return 1
+        fi
+    fi
+
+    # Check free memory
+    free_memory=$(free -m | awk '/^Mem:/{print $7}')
+    if [ $? -ne 0 ]; then
+        echo "Failed to check free memory."
+        CHECKLIST+=("Free memory check: Failed")
+        return 1
+    fi
+
+    if [ $free_memory -ge 500 ]; then
+        echo "Free memory: Passed ($free_memory MB available)"
+        CHECKLIST+=("Free memory: Passed")
+    else
+        echo "Free memory: Failed ($free_memory MB available)"
+        CHECKLIST+=("Free memory: Failed ($free_memory MB available)")
+    fi
+
+    # Check CPU
+    mpstat_output=$(mpstat 1 1 | awk '/^Average:/ {print $12}')
+    if [ $? -ne 0 ]; then
+        echo "Failed to check CPU idle percentage."
+        CHECKLIST+=("CPU idle check: Failed")
+        return 1
+    fi
+
+    cpu_idle=$(echo $mpstat_output | awk '{print $1}')
+    cpu_cores=$(nproc)
+    if [ $? -ne 0 ]; then
+        echo "Failed to check CPU cores."
+        CHECKLIST+=("CPU cores check: Failed")
+        return 1
+    fi
+
+    cpu_free=$(echo "100 - $cpu_idle" | bc)
+    if [ $? -ne 0 ]; then
+        echo "Failed to calculate CPU free percentage."
+        CHECKLIST+=("CPU free calculation: Failed")
+        return 1
+    fi
+
+    echo "CPU Idle: $cpu_idle%"
+    echo "CPU Free: $cpu_free%"
+
+    if [ $cpu_cores -ge 2 ] && (( $(echo "$cpu_free >= .25" | bc -l) )); then
+        echo "CPU: Passed ($cpu_cores cores, $cpu_free% free)"
+        CHECKLIST+=("CPU: Passed")
+    else
+        echo "CPU: Failed ($cpu_cores cores, $cpu_free% free)"
+        CHECKLIST+=("CPU: Failed ($cpu_cores cores, $cpu_free% free)")
+    fi   
+
+    # Check /home/ space
+    home_free=$(df -m /home | awk 'NR==2 {print $4}')
+    if [ $? -ne 0 ]; then
+       
+        echo "Failed to check /home/ space."
+        CHECKLIST+=("/home/ space check: Failed")
+        return 1
+    fi
+
+    if [ $home_free -gt 500 ]; then
+        echo "/home/ space: Passed ($home_free MB available)"
+        CHECKLIST+=("/home/ space: Passed")
+    else
+        echo "/home/ space: Failed ($home_free MB available)"
+        CHECKLIST+=("/home/ space: Failed ($home_free MB available)")
+    fi
+
+    # Check /opt/ space
+    opt_free=$(df -m /opt | awk 'NR==2 {print $4}')
+    if [ $? -ne 0 ]; then
+        echo "Failed to check /opt/ space."
+        CHECKLIST+=("/opt/ space check: Failed")
+        return 1
+    fi
+
+    if [ $opt_free -gt 2000 ]; then
+        echo "/opt/ space: Passed ($opt_free MB available)"
+        CHECKLIST+=("/opt/ space: Passed")
+    else
+        echo "/opt/ space: Failed ($opt_free MB available)"
+        CHECKLIST+=("/opt/ space: Failed ($opt_free MB available)")
+    fi
+}
+
+# Function to check if user is in sudoers list
+check_sudoers() {
+    echo "Checking sudoers configuration..."
+
+    # Check if the user exists
+    if ! id "$ssh_user" &>/dev/null; then
+        echo "User $ssh_user does not exist."
+        CHECKLIST+=("User existence check: Failed")
+        return 1
+    fi
+
+    sudo_group=$(id -nG "$ssh_user" | grep -w "sudo")
+    if [ $? -ne 0 ]; then
+        echo "Failed to check if user is in sudo group."
+        CHECKLIST+=("Sudo group check: Failed")
+        return 1
+    fi
+
+    if [ -z "$sudo_group" ]; then
+        echo "User $ssh_user is not in the sudo group."
+        CHECKLIST+=("User sudo group membership: Failed")
+    else
+        echo "User $ssh_user is in the sudo group."
+        CHECKLIST+=("User sudo group membership: Passed")
+    fi
+
+    # Update sudoers file
+    sudoers_entry="$ssh_user ALL=(ALL) NOPASSWD:/opt/.ch-tools/*/*/*, /opt/.ch-tools/*/*, /bin/mkdir, /bin/echo, /bin/chmod 755 /opt/.ch-tools/*, /bin/chmod 755 /home/$ssh_user/chcmd, /bin/chmod -R 755 /opt/.ch-tools, /bin/chmod -R 755 /home/$ssh_user/.ch-tools, /bin/chown $ssh_user\: /opt/.ch-tools/*, /bin/chown -R $ssh_user\: /home/$ssh_user/chcmd, /bin/chown -R $ssh_user\: /opt/.ch-tools, /bin/chown -R $ssh_user\: /home/$ssh_user/.ch-tools"
+
+    if ! sudo grep -qF "$sudoers_entry" /etc/sudoers; then
+        echo "Adding sudoers entry..."
+        echo "$sudoers_entry" | sudo EDITOR='tee -a' visudo
+        if [ $? -eq 0 ]; then
+            echo "Sudoers entry added successfully."
+            CHECKLIST+=("Sudoers entry: Passed")
+        else
+            echo "Failed to add sudoers entry."
+            CHECKLIST+=("Sudoers entry: Failed")
+        fi
+    else
+        echo "Sudoers entry already exists."
+        CHECKLIST+=("Sudoers entry: Passed")
+    fi
+
+    # Disable requiretty for the user
+    if ! sudo grep -q "Defaults:$ssh_user    !requiretty" /etc/sudoers; then
+        echo "Disabling requiretty for $ssh_user..."
+        echo "Defaults:$ssh_user    !requiretty" | sudo EDITOR='tee -a' visudo
+        if [ $? -eq 0 ]; then
+            echo "requiretty disabled for $ssh_user."
+            CHECKLIST+=("requiretty disable: Passed")
+        else
+            echo "Failed to disable requiretty for $ssh_user."
+            CHECKLIST+=("requiretty disable: Failed")
+        fi
+    else
+        echo "requiretty is already disabled for $ssh_user."
+        CHECKLIST+=("requiretty disable: Passed")
+    fi
+}
+
+# Function to check if SSHD is running
+check_sshd() {
+    echo "Checking if SSHD is running..."
+    sshd_status=$(ps ax | grep -v grep | grep sshd)
+    if [ -z "$sshd_status" ]; then
+        echo "SSHD is not running."
+        CHECKLIST+=("SSHD running check: Failed")
+    else
+        echo "SSHD is running."
+        CHECKLIST+=("SSHD running check: Passed")
+    fi
+}
+
+# Function to ensure port 443 is open
+check_port_443() {
+    echo "Checking if port 443 is open..."
+
+    # Check if port 443 is open
+    if ss -tuln | grep -q ':443 '; then
+        echo "Port 443 is open."
+        CHECKLIST+=("Port 443 open check: Passed")
+    else
+        echo "Port 443 is not open. Enabling port 443..."
+
+        # Check if ufw is installed
+        if ! command -v ufw &> /dev/null; then
+            echo "ufw is not installed. Installing ufw..."
+            sudo apt-get install -y ufw
+            if [ $? -ne 0 ]; then
+                echo "Failed to install ufw."
+                CHECKLIST+=("ufw installation: Failed")
+                return 1
+            fi
+        fi
+
+        sudo ufw allow 443/tcp
+        sudo ufw reload
+        if [ $? -eq 0 ]; then
+            echo "Port 443 has been enabled."
+            CHECKLIST+=("Port 443 enable: Passed")
+        else
+            echo "Failed to enable port 443."
+            CHECKLIST+=("Port 443 enable: Failed")
+        fi
+    fi
+
+    # Enable ufw if it's not enabled
+    if ! sudo ufw status | grep -qw "active"; then
+        echo "Enabling firewall..."
+        sudo ufw enable
+        if [ $? -eq 0 ]; then
+            echo "Firewall has been enabled."
+            CHECKLIST+=("Firewall enable: Passed")
+        else
+            echo "Failed to enable the firewall."
+            CHECKLIST+=("Firewall enable: Failed")
+        fi
+    else
+        CHECKLIST+=("Firewall enable: Passed")
+    fi
+}
+
+# Main script execution
+check_resources
+check_sudoers
+check_sshd
+check_port_443
+
+# Display checklist
+echo "Checklist for requirements:"
+for item in "${CHECKLIST[@]}"; do
+    echo "- $item"
+done
---
+++ WindowsHost.ps1
@@ -0,0 +1,232 @@
+# Check CPU and Memory resources
+function Check-Resources {
+    $memory = (Get-WmiObject Win32_OperatingSystem).FreePhysicalMemory / 1KB
+    $cpu = (Get-WmiObject Win32_Processor).NumberOfLogicalProcessors
+    $disk = (Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace / 1GB
+
+    Write-Output "Checking CPU and Memory resources..."
+    if ($memory -ge 500 -and $cpu -ge 2 -and $disk -ge 20) {
+        Write-Output "CPU and Memory resources check: Passed"
+        return $true
+    } else {
+        Write-Output "CPU and Memory resources check: Failed"
+        Write-Output "Please ensure at least 500 MB of free memory, 2 logical processors, and 20 GB of free disk space."
+        return $false
+    }
+}
+
+# Check PowerShell version
+function Check-PowerShellVersion {
+    $psVersionDetails = $PSVersionTable.PSVersion
+    $osVersion = [System.Version](Get-WmiObject Win32_OperatingSystem).Version
+
+    Write-Output "Checking PowerShell version..."
+    Write-Output "OS Version: $osVersion"
+
+    if ($osVersion -lt [System.Version]"6.1.7600" -and $psVersionDetails.Major -lt 3) {
+        Write-Output "Windows Server 2008 R2 detected. PowerShell 3.0 is required."
+        Write-Output "Please download and install PowerShell 3.0 from the following link:"
+        Write-Output "https://www.microsoft.com/en-us/download/details.aspx?id=34595"
+        Write-Output "After installation and rebooting, please run the script again."
+        return $false
+    } elseif ($psVersionDetails.Major -ge 3) {
+        Write-Output "PowerShell version check: Passed"
+        Write-Output "PowerShell Version: $($psVersionDetails.Major).$($psVersionDetails.Minor).$($psVersionDetails.Build).$($psVersionDetails.Revision)"
+        return $true
+    } else {
+        Write-Output "PowerShell version check: Failed"
+        Write-Output "Minimum PowerShell version required: 3"
+        return $false
+    }
+}
+
+# Set remote execution policy
+function Set-RemoteExecutionPolicy {
+    Write-Output "Setting remote execution policy..."
+    $executionPolicy = Get-ExecutionPolicy -Scope LocalMachine
+    if ($executionPolicy -ne "RemoteSigned") {
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
+        Write-Output "Execution policy updated to RemoteSigned"
+    } else {
+        Write-Output "Execution policy: RemoteSigned"
+    }
+}
+
+# Ensure port 443 is open
+function Ensure-Port443Open {
+    Write-Verbose "Checking if port 443 is open..."
+    $portCheck = netstat -an | Select-String -Pattern "0.0.0.0:443"
+
+    if ($portCheck) {
+        Write-Output "Port 443 (HTTPS) is already open."
+    }
+    else {
+        Write-Verbose "Port 443 (HTTPS) is not open. Opening port 443..."
+        try {
+            New-NetFirewallRule -DisplayName "Open Port 443" -Direction Inbound -LocalPort 443 -Protocol TCP -Action Allow
+            Write-Output "Port 443 has been opened."
+        }
+        catch {
+            Write-Output "Failed to open port 443. Error: $_"
+        }
+    }
+}
+
+# Check TrustedHosts policy configuration
+function Check-TrustedHostsPolicy {
+    Write-Output "Checking TrustedHosts policy configuration..."
+
+    try {
+        Import-Module GroupPolicy -ErrorAction Stop
+        $trustedHostsPolicy = Get-GPRegistryValue -Name 'RemoteHosts' -Key 'HKLM\Software\Policies\Microsoft\Windows\WinRM\Client' -ValueName 'TrustedHosts' -ErrorAction SilentlyContinue
+
+        if ($trustedHostsPolicy -ne $null -and $trustedHostsPolicy.PolicyState -eq "Enabled") {
+            Write-Output "TrustedHosts policy is managed by Group Policy. Setting it to 'Not Configured'."
+
+            # Set the TrustedHosts policy to 'Not Configured'
+            Set-GPRegistryValue -Name 'RemoteHosts' -Key 'HKLM\Software\Policies\Microsoft\Windows\WinRM\Client' -ValueName 'TrustedHosts' -Type String -Value '' -ErrorAction Stop
+            Remove-GPRegistryValue -Name 'RemoteHosts' -Key 'HKLM\Software\Policies\Microsoft\Windows\WinRM\Client' -ValueName 'TrustedHosts' -ErrorAction Stop
+
+            # Update Group Policy
+            Write-Output "Updating Group Policy to reflect changes..."
+            gpupdate /force | Out-Null
+
+            Write-Output "TrustedHosts policy has been set to 'Not Configured'."
+        } else {
+            Write-Output "TrustedHosts policy is not managed by Group Policy. No change required."
+        }
+    }
+    catch {
+        Write-Output "Error checking or updating TrustedHosts policy: $_"
+        Write-Output "GroupPolicy module is not available. Skipping TrustedHosts policy check."
+        Write-Output "GroupPolicy module is not available. Skipping TrustedHosts policy check."
+    }
+}
+
+# Configure WinRM
+function Configure-WinRM {
+    Write-Verbose "Configuring WinRM..."
+
+    $scriptUrl = "https://ch-public-bucket.s3.amazonaws.com/ConfigureRemoting.ps1"
+    $scriptPath = "$env:TEMP\ConfigureRemoting.ps1"
+
+    if (-Not (Test-Path $scriptPath)) {
+        try {
+            Write-Verbose "Downloading ConfigureRemoting.ps1 from $scriptUrl..."
+            Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath -ErrorAction Stop
+            if ($VerbosePreference -eq 'Continue') {
+                Write-Output "ConfigureRemoting.ps1 downloaded successfully to $scriptPath."
+            }
+        }
+        catch {
+         Write-Output "Failed to download ConfigureRemoting.ps1 script. Error: $_"
+        return $false
+    }
+}
+
+try {
+    Write-Verbose "Executing ConfigureRemoting.ps1 script at $scriptPath..."
+    $configOutput = & $scriptPath -Verbose:$VerbosePreference 4>&1
+    if ($VerbosePreference -eq 'Continue') {
+        Write-Output "Output from ConfigureRemoting.ps1:`n$configOutput"
+    }
+    return $true
+}
+catch {
+    Write-Output "Failed to execute ConfigureRemoting.ps1 script. Error: $_"
+    return $false
+}
+}
+
+# Disable UAC remote restrictions
+function Disable-UACRestrictions {
+    Write-Output "Disabling UAC remote restrictions..."
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 1
+}
+
+# Main script
+Write-Output "Starting resource check..."
+$resourceCheckPassed = $false
+$psVersionCheckPassed = $false
+$port443CheckPassed = $false
+$winRMConfigPassed = $false
+
+# Starting resource check
+$resourcesCheck = Check-Resources
+if (-not $resourcesCheck) {
+    Write-Output "Script execution halted due to insufficient CPU or Memory resources."
+    exit
+}
+
+$resourceCheckPassed = $true
+
+# Starting PowerShell version check
+$psVersionCheck = Check-PowerShellVersion
+if (-not $psVersionCheck) {
+    Write-Output "Script execution halted due to failed PowerShell version check."
+    exit
+}
+
+$psVersionCheckPassed = $true
+
+# Starting remote execution policy setup
+Set-RemoteExecutionPolicy
+
+# Starting port 443 check
+$port443Check = Ensure-Port443Open
+if (-not $port443Check) {
+    Write-Output "Port 443 (HTTPS) check failed. Script execution halted."
+    exit
+}
+
+$port443CheckPassed = $true
+
+# Starting TrustedHosts policy check
+Check-TrustedHostsPolicy
+
+# Starting WinRM configuration
+$winRMConfig = Configure-WinRM
+if (-not $winRMConfig) {
+    Write-Output "WinRM configuration failed. Script execution halted."
+    exit
+}
+
+$winRMConfigPassed = $true
+
+# Starting UAC remote restrictions disable
+Disable-UACRestrictions
+
+Write-Output "All checks completed."
+
+# Generate checklist
+Write-Output "`nChecklist:`n"
+
+if ($resourceCheckPassed) {
+    Write-Output "- CPU and Memory resources check: Passed"
+} else {
+    Write-Output "- CPU and Memory resources check: Failed"
+}
+
+if ($psVersionCheckPassed) {
+    Write-Output "- PowerShell version check: Passed"
+} else {
+    Write-Output "- PowerShell version check: Failed"
+}
+
+if ($port443CheckPassed) {
+    Write-Output "- Port 443 (HTTPS) check: Passed"
+} else {
+    Write-Output "- Port 443 (HTTPS) check: Failed"
+}
+
+# TrustedHosts policy check doesn't return a pass/fail status directly, so assuming if it reaches here, it passed
+Write-Output "- TrustedHosts policy check: Passed"
+
+if ($winRMConfigPassed) {
+    Write-Output "- WinRM configuration: Passed"
+} else {
+    Write-Output "- WinRM configuration: Failed"
+}
+
+Write-Output "- UAC remote restrictions disable: Passed"
+          
